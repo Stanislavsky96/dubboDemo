e478545f91356f3db4f529bde5685746ee600889
Stanislaviski <1769377985@qq.com>
2019-04-01 21:36:05 +0800

    update 2019/4/01

--- dubboConsumer/server/src/main/java/com/chris/server/controller/ItemController.java
+++ dubboConsumer/server/src/main/java/com/chris/server/controller/ItemController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.PathParam;
+
 @RestController
 @RequestMapping("/item")
 public class ItemController {
@@ -20,6 +22,14 @@ public class ItemController {
     @GetMapping("/list")
     public BaseResponse getItemList(){
         BaseResponse res= itemService.listItems();
+        return res;
+    }
+
+    @GetMapping("/search")
+    public BaseResponse getSearchRes(@PathParam(value = "pageNum") Integer pageNum,
+                                     @PathParam(value = "pageSize") Integer pageSize,
+                                     @PathParam(value = "name") String name){
+        BaseResponse res=itemService.listPageItemsParams(pageNum,pageSize,name);
         return res;
     }
 }
